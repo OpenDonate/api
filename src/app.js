297689b97f7d03555cfeff4aae9f85b035af1996
zameschua <zames.chua@gmail.com>
2020-01-18 15:37:28 +0800

    Introduce subdomains

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
+var subdomain = require('express-subdomain');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/index');
 
 var app = express();
 
@@ -12,9 +13,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(subdomain('app', express.static(path.join(__dirname, 'public'))));
 
-app.use('/', indexRouter);
+app.use(subdomain('api', apiRouter));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
