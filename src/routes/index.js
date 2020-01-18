1e6318e6ccba8e73cfec968b43f3124a84c7a364
zameschua <zames.chua@gmail.com>
2020-01-18 21:03:24 +0800

    Merge with Cheewee's changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/healthcheck', function(req, res, next) {
-  res.send('Yo');
-});
+// /* GET home page. */
+// router.get('/healthcheck', function(req, res, next) {
+//   res.send('Yo');
+// });
 
-router.post('/transactions', function(req, res, next) {
-  res.send('To implement later');
-});
+// router.post('/transactions', function(req, res, next) {
+//   res.send('To implement later');
+// });
 
-module.exports = router;
+// module.exports = router;
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const fs = require('./xxxxx');
+var express = require('express');
+var router = express.Router();
+const fs = require('./file_modifier');
 const Git = require("nodegit");
-//const guides = require('./asdf')
 
-const app = express();
-const port = 3000;
-
-//app.use('/guide', guides)
-app.use(express.json())
+// router.use(express.json())
 
 let merch_donate_handler = (req, res) => {
 	// 1) Create a transaction of type string with values from req
 	const transaction = Object.values(req.body).join(',')
 	console.log('Transaction: ' + transaction)
-	
+
 	const fn = "zames.txt"
 	let numLines = fs.count_file_lines(fn)
 	if(numLines == -1) {
@@ -26,33 +22,19 @@ let merch_donate_handler = (req, res) => {
 		// Commit file
 		//console.log(numLines)
 	} else {
-		console.log("rawr");
 		// Write to file
 		console.log(numLines)
 		fs.append_to_file(fn, transaction)
-		commit_file('./gitchain', fn)
 	}
-	
+
 	res.status(200).send('OK')
 }
 
-let home_handler = (req, res) => {
-	res.status(200).send('OK')
-}
+router.post('/merchant/id/donate', merch_donate_handler)
 
-/**
-app.post('/customer/signup',cust_signup_handler)
-app.post('/customer/login',cust_login_handler)
-app.get('/customer/custId/past-transactions',cust_history_handler)
-app.get('/customer/custId/past-transactions/transacID',cust_transaction_handler)
+/* GET home page. */
+router.get('/healthcheck', function(req, res, next) {
+  res.send('Yo');
+});
 
-app.post('/merchant/signup',cust_signup_handler)
-app.post('/merchant/login',cust_login_handler)
-app.post('/merchant/id/donate', merch_donate_handler)//main merchant end-point
-app.get('/merchant/id/past-transactions', merch_transaction_history_handler)
-app.get('/merchant/id/past-transactions/transacID', merch_transaction_handler)
-**/
-app.post('/merchant/id/donate', merch_donate_handler)//main merchant end-point
-app.get('/', home_handler)
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = router;
