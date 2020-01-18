73a545497abd514398354ee52dd3706c60df5ce0
zameschua <zames.chua@gmail.com>
2020-01-18 16:42:12 +0800

    Install Typescript for frontend

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ var apiRouter = require('./routes/index');
 
 var app = express();
 
+app.set('view engine', 'html');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
